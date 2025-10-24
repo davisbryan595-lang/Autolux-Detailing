296d7a27d52ec06b648541e29e2fffef9b426a5e
Davis Bryan <davisbryan595@gmail.com>
2025-10-24 12:43:52 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
-import Loader from "@/components/Loader";
+import Loader from "@/components/page-Loader";
+
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
